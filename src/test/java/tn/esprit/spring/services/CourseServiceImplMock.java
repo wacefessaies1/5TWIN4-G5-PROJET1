c712854c6e693af27cea49333ef45cdb3b81711c
emnahammami <43586779+emnahammami@users.noreply.github.com>
2023-11-07 00:00:22 +0100

    JUNIT +MOCKITO fix

--- src/test/java/tn/esprit/spring/services/CourseServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/CourseServiceImplMock.java
@@ -1,4 +1,6 @@
 package tn.esprit.spring.services;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -6,13 +8,31 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Support;
+import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.repositories.ICourseRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class CourseServiceImplMock {
-	@Mock
-	ICourseRepository courseRepository;
-	// Ou
-	@InjectMocks
-	CourseServicesImpl courseservice;
+
+    @Mock
+    ICourseRepository courseRepository;
+
+    @InjectMocks
+    CourseServicesImpl courseservice;
+
+    @BeforeEach
+    public void setUp() {
+        List<Course> listCourses = new ArrayList<>();
+        listCourses.add(new Course(1L, 0, TypeCourse.INDIVIDUAL, Support.SKI, null, 0, null));
+        listCourses.add(new Course(2L, 0, TypeCourse.COLLECTIVE_ADULT, Support.SNOWBOARD, null, 0, null));
+
+        // Définissez le comportement attendu pour le repository mock
+        Mockito.when(courseRepository.findAll()).thenReturn(listCourses);
+    }
 }
