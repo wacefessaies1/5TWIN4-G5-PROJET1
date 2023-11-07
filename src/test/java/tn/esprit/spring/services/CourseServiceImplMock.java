d54422d6e77eb21a8c363e229f7744075ffd0f9f
emnahammami <43586779+emnahammami@users.noreply.github.com>
2023-11-07 15:24:08 +0100

    fix mockito

--- src/test/java/tn/esprit/spring/services/CourseServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/CourseServiceImplMock.java
@@ -16,6 +16,11 @@ import tn.esprit.spring.repositories.ICourseRepository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class CourseServiceImplMock {
@@ -26,13 +31,22 @@ public class CourseServiceImplMock {
     @InjectMocks
     CourseServicesImpl courseservice;
 
-    @BeforeEach
-    public void setUp() {
-        List<Course> listCourses = new ArrayList<>();
-        listCourses.add(new Course(1L, 0, TypeCourse.INDIVIDUAL, Support.SKI, null, 0, null));
-        listCourses.add(new Course(2L, 0, TypeCourse.COLLECTIVE_ADULT, Support.SNOWBOARD, null, 0, null));
+    @Test
+    public void testFindAllCourses() {
+        // Créez un objet Course ou une liste de courses factices pour le résultat attendu
 
-        // Définissez le comportement attendu pour le repository mock
-        Mockito.when(courseRepository.findAll()).thenReturn(listCourses);
+        // Définissez le comportement attendu pour le mock courseRepository, par exemple :
+        List<Course> expectedCourses = new ArrayList<>();
+        expectedCourses.add(new Course(1L, 0, TypeCourse.INDIVIDUAL, Support.SKI, null, 0, null));
+        expectedCourses.add(new Course(2L, 0, TypeCourse.COLLECTIVE_ADULT, Support.SNOWBOARD, null, 0, null));
+
+        Mockito.when(courseRepository.findAll()).thenReturn(expectedCourses);
+
+        // Appelez la méthode à tester
+        List<Course> actualCourses = courseservice.retrieveAllCourses();
+
+        // Effectuez des assertions pour vérifier le comportement de la méthode
+        assertEquals(expectedCourses.size(), actualCourses.size());
+        // Ajoutez d'autres assertions si nécessaire pour vérifier les détails des objets Course, etc.
     }
 }
